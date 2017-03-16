38fbe739d78fc0e41f687f2d189f56386a6c57ac
Billy Everyteen <joaopcbertoldo@gmail.com>
2017-03-16 02:12:36 -0300

    arduino web server test

--- TesteArduinoSerialCom/HttpSenderTest/Program.cs
+++ TesteArduinoSerialCom/HttpSenderTest/Program.cs
@@ -13,20 +13,28 @@ namespace HttpSenderTest
             Console.Title = "HTTP SENDER TEST";
             Console.SetWindowSize(150, 15);
             Thread.Sleep(3000);
-            string baseAddress1 = "http://localhost:8081/testapi";
-            string baseAddress3 = $"http://{ipv4}:8081/testapi";
-            string baseAddress4 = $"http://{Environment.MachineName}:8081";
-            SendRequest(baseAddress1);
-            SendRequest(baseAddress3);
-            SendRequest(baseAddress4);
+            var url1 = "http://localhost:8081/testapi";
+            var url3 = $"http://{ipv4}:8081/testapi";
+            var url4 = $"http://{Environment.MachineName}:8081/testapi";
+            var arduinoUrl1 = "http://esp8266/";
+            var arduinoUrl2 = "http://192.168.0.103/";
+            var arduinoUrl3 = "http://esp8266/inline";
+            var arduinoUrl4 = "http://192.168.0.103/inline";
+            SendRequest(url1);
+            SendRequest(url3);
+            SendRequest(url4);
+            SendRequest(arduinoUrl1);
+            SendRequest(arduinoUrl2);
+            SendRequest(arduinoUrl3);
+            SendRequest(arduinoUrl4);
             Console.ReadKey();
         }
 
-        private static void SendRequest(string requestUriString)
+        private static void SendRequest(string url)
         {
             try {
-                Console.WriteLine($"------------------------------- START {requestUriString} -----------------------------------------");
-                WebRequest request = WebRequest.Create(requestUriString);
+                Console.WriteLine($"------------------------------- START {url} -----------------------------------------");
+                WebRequest request = WebRequest.Create(url);
                 // Set the Method property of the request to GET.
                 request.Method = "GET";
                 // Get the response.
@@ -44,17 +52,19 @@ namespace HttpSenderTest
                 reader.Close();
                 dataStream.Close();
                 response.Close();
-                Console.WriteLine($"------------------------------- OK END {requestUriString} -----------------------------------------");
+                Console.WriteLine($"------------------------------- OK END {url} -----------------------------------------");
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine();
             } catch (Exception ex) {
                 Console.WriteLine(ex);
-                Console.WriteLine($"------------------------------- EXCEPTION END {requestUriString} -----------------------------------------");
+                Console.WriteLine($"------------------------------- EXCEPTION END {url} -----------------------------------------");
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine();
             }
         }
+        
+
     }
 }
